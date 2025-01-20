a807d9864bf72e750bcbf9f1559cd2ab5c8262b6
myu <23029169@myrp.edu.sg>
2025-01-20 15:41:15 +0800

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import 'dotenv/config'; // Load environment variables
+import express from 'express';
+import mongoose from 'mongoose';
+import workoutRoutes from './routes/workouts.js'; // Ensure to include the .js extension
+import cors from 'cors';
+
+// Express app
+const app = express();
+
+app.use(cors());
+
+// Middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// Routes
+app.use('/api/workouts', workoutRoutes);
+
+// Connect to the database
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to database');
+    // Listen on port
+    app.listen(process.env.PORT, () => {
+      console.log('Listening for requests on port', process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
